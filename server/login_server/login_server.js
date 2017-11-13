e9f84f0b1ac6c92c92ceeb097bb9ba58311d9c02
aoyaowang <415506575@qq.com>
2017-11-13 11:00:58 +0800

    add

--- server/login_server/login_server.js
+++ server/login_server/login_server.js
@@ -11,6 +11,7 @@ var enums = require("../utils/enums");
 var crypto = require("crypto");
 var Buffer = require("buffer").Buffer;
 
+var exec = require('child_process').exec; 
 
 function md5(data){
     var buf = new Buffer(data);
@@ -310,6 +311,21 @@ app.get("/getlog", function(req, res){
         }
 
         send(res, {code: enums.CODE.SUC_OK, data: data});
+    });
+});
+
+app.get("/upweb", function(req, res){
+    exec("cd /home/rec/ && git pull && nginx -s reload", function(err,stdout,stderr){
+        if(err) {
+            console.log('get weather api error:'+stderr);
+        } else {
+            /*
+            这个stdout的内容就是上面我curl出来的这个东西：
+            {"weatherinfo":{"city":"北京","cityid":"101010100","temp":"3","WD":"西北风","WS":"3级","SD":"23%","WSE":"3","time":"21:20","isRadar":"1","Radar":"JC_RADAR_AZ9010_JB","njd":"暂无实况","qy":"1019"}}
+            */
+            var data = JSON.parse(stdout);
+            console.log(data);
+        }
     });
 });
 
