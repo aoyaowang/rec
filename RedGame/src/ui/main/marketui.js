074300d128dddf4c4f3ff6021bd5f6bc8bd2ee1d
aoyaowang <415506575@qq.com>
2017-11-14 16:02:29 +0800

    add

--- RedGame/src/ui/main/marketui.js
+++ RedGame/src/ui/main/marketui.js
@@ -25,12 +25,30 @@ var marketUI = cc.Layer.extend({
     },
     onEnter:function() {
         this._super();
+        Client.addMap("billfangka", this);
     },
     onExit:function() {
         this._super();
+        Client.removeMap("billfangka", this);
     },
     goldClick:function(sender) {
         var m = sender.getTag();
-
+        if (parseInt(m) <= 0) return;
+        Server.send("billfangka", {t: RoleInfo.token, bill: m});
+    },
+    billfangka:function(msg){
+        if (msg.code != 0) return;
+        var data = msg.data;
+        if (isWeiXin()) {
+            var timestamp = Date.parse(new Date()) / 1000;
+            WeixinJSBridge.invoke(
+                'getBrandWCPayRequest', data,
+                function(res){
+                    if(res.err_msg == "get_brand_wcpay_request:ok" ) {
+                        console.log("支付成功")
+                    }     // 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回    ok，但并不保证它绝对可靠。
+                }
+            );
+        }
     }
 });
