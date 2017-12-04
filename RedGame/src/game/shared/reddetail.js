580e75fe2b0accad5fdf6fd095b9254981c6e5ea
aoyaowang <415506575@qq.com>
2017-12-04 09:57:05 +0800

    k

--- RedGame/src/game/shared/reddetail.js
+++ RedGame/src/game/shared/reddetail.js
@@ -134,7 +134,7 @@ var reddetailUI = ccui.Widget.extend({
 
         var zset = true;
         var zqiang = 0;
-        var zpei = 0;
+        var zpei = -1 *parseInt(this.m_coin) / 100;
 
         for (var key in this.m_data) {
             var p = this.m_data[key];
@@ -150,10 +150,10 @@ var reddetailUI = ccui.Widget.extend({
                     }
                     var ql = cc.color(0,255,0);
                     var pei = "赔付:";
-                    var pei0 = (this.m_red.bomb == p.data.last ? "-" + (parseInt((parseInt(this.m_coin) * 1.5)) / 100) : 0);
+                    var pei0 = (this.m_red.bomb == p.data.last && this.m_red.owner.uid == p.data.uid ? "-" + (parseInt((parseInt(this.m_coin) * 1.5)) / 100) : 0);
                     var pel = pei0 != 0 ? cc.color(255, 0, 0) : cc.color(0,255,0);
-                    if (this.m_red.bomb == p.data.last) {
-                        zpei += (parseInt(this.m_coin * (this.m_num == 7 ? 1.5:1.0)) / 100) - parseInt(this.m_coin) / 100;
+                    if (this.m_red.bomb == p.data.last && this.m_red.owner.uid == p.data.uid) {
+                        zpei += (parseInt(this.m_coin * (this.m_num == 7 ? 1.5:1.0)) / 100);
                     }
                     var mp = parseInt(parseInt(p.data.qiang) * 0.03) / 100;
                     if (mp < 0.01) mp = 0;
